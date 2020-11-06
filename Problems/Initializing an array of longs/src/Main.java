2744b7fabd85e3df4999ce778a0f858ea4a6dc08
Alekseiy <alekseiyfadeev@gmail.com>
2020-11-06 16:01:27 +0300

    Stage 2/5 implemented.

---
+++ Problems/Initializing an array of longs/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long[] longNumbers;
+
+        longNumbers = new long[] {100_000_000_001L, 100_000_000_002L, 100_000_000_003L};
+
+        System.out.println(Arrays.toString(longNumbers));
+
+        //char[] array1 = new char[-1];
+        char[] array2 = new char[1];
+        char[] array3 = new char[0];
+
+        char[] array5 = { 'a', 'b', 'c', 'd' };
+    }
+}
--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,40 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        char[][] fieldState;
+        Scanner stdin = new Scanner(System.in);
+
+        fieldState = readFieldState(stdin);
+        System.out.println(formatFieldState(fieldState));
+    }
+
+    private static String formatFieldState(char[][] fieldState) {
+        StringBuilder ret = new StringBuilder();
+        ret.append("---------\n");
+        for (int i = 0; i < 3; i++) {
+            ret.append("| ");
+            for (int j = 0; j < 3; j++) {
+                ret.append(fieldState[i][j]).append(" ");
+            }
+            ret.append("|\n");
+        }
+        ret.append("---------\n");
+        return ret.toString();
+    }
+
+    private static char[][] readFieldState(Scanner in) {
+        char[][] ret = new char[3][3];
+        String str = in.next();
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                ret[i][j] = str.charAt(k);
+                k++;
+            }
+        }
+        return ret;
     }
 }
